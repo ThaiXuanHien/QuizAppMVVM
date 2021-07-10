5649edd24b3f727169e40094fb0bb8d18335a628
thaixuanhien <xtit6789@gmail.com>
2021-07-10 22:46:54 +0700

    update

--- app/src/main/java/com/hienthai/quizappmvvm/QuizFragment.java
+++ app/src/main/java/com/hienthai/quizappmvvm/QuizFragment.java
@@ -1,6 +1,7 @@
 package com.hienthai.quizappmvvm;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,17 +14,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class QuizFragment extends Fragment {
+public class QuizFragment extends Fragment implements View.OnClickListener {
 
+    private static final String TAG = "QUIZ_FRAGMENT_LOG";
     private TextView txt_title_quiz, txt_quiz_question_number, txt_quiz_question_time, txt_quiz_question, txt_quiz_question_feedback;
     private ProgressBar pgb_quiz_question;
     private Button btn_quiz_option_one, btn_quiz_option_two, btn_quiz_option_three, btn_next_question;
@@ -34,6 +33,11 @@ public class QuizFragment extends Fragment {
     private List<Question> questionList = new ArrayList<>();
     private List<Question> questionListToAnswer = new ArrayList<>();
     private long totalQuestions = 0L;
+
+    private CountDownTimer countDownTimer;
+
+    private boolean canAnswer = false;
+    private int currentQuestion = 0;
 
     public QuizFragment() {
         // Required empty public constructor
@@ -96,6 +100,11 @@ public class QuizFragment extends Fragment {
             }
         });
 
+        //Set Button Click Listeners
+        btn_quiz_option_one.setOnClickListener(this);
+        btn_quiz_option_two.setOnClickListener(this);
+        btn_quiz_option_three.setOnClickListener(this);
+
     }
 
     private void loadDataToUI() {
@@ -112,14 +121,48 @@ public class QuizFragment extends Fragment {
         txt_quiz_question_number.setText(questNum + "");
 
         //Load Question Text
-        txt_quiz_question.setText(questionListToAnswer.get(questNum).getQuestion());
+        txt_quiz_question.setText(questionListToAnswer.get(questNum - 1).getQuestion());
 
         //Load Options
-        btn_quiz_option_one.setText(questionListToAnswer.get(questNum).getOptionA());
-        btn_quiz_option_two.setText(questionListToAnswer.get(questNum).getOptionB());
-        btn_quiz_option_three.setText(questionListToAnswer.get(questNum).getOptionC());
+        btn_quiz_option_one.setText(questionListToAnswer.get(questNum - 1).getOptionA());
+        btn_quiz_option_two.setText(questionListToAnswer.get(questNum - 1).getOptionB());
+        btn_quiz_option_three.setText(questionListToAnswer.get(questNum - 1).getOptionC());
+        canAnswer = true;
+        currentQuestion = questNum;
 
+        //Start Question Timer
+        startTimer(questNum);
+    }
 
+    private void startTimer(int questionNumber) {
+
+        //Set Timer Text
+        final Long timeToAnswer = questionListToAnswer.get(questionNumber - 1).getTimer();
+        txt_quiz_question_time.setText(timeToAnswer.toString());
+
+        //Show Timer ProgressBar
+        pgb_quiz_question.setVisibility(View.VISIBLE);
+
+        //Start CountDown
+        countDownTimer = new CountDownTimer(timeToAnswer * 1000, 10) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                //Update Time
+                txt_quiz_question_time.setText(millisUntilFinished / 1000 + "");
+
+                //Progress in percent
+                Long percent = millisUntilFinished / (timeToAnswer * 10);
+                pgb_quiz_question.setProgress(percent.intValue());
+            }
+
+            @Override
+            public void onFinish() {
+                //Time Up, Cannot Answer Question Anymore
+                canAnswer = false;
+            }
+        };
+
+        countDownTimer.start();
     }
 
     private void enableOption() {
@@ -151,5 +194,35 @@ public class QuizFragment extends Fragment {
 
     private int getRandomInt(int min, int max) {
         return ((int) (Math.random() * (max - min))) + min;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_quiz_option_one:
+                verifyAnswer(btn_quiz_option_one);
+                break;
+            case R.id.btn_quiz_option_two:
+                verifyAnswer(btn_quiz_option_two);
+                break;
+            case R.id.btn_quiz_option_three:
+                verifyAnswer(btn_quiz_option_three);
+                break;
+        }
+    }
+
+    private void verifyAnswer(Button selectedAnswer) {
+        //Check Answer
+        if (canAnswer) {
+            if (questionListToAnswer.get(currentQuestion - 1).getAnswer().equals(selectedAnswer.getText())) {
+                //Correct Answer
+                Log.d(TAG, "Correct Answer");
+            } else {
+                //Wrong Answer
+                Log.d(TAG, "Wrong Answer");
+            }
+            //Set Can answer to false
+            canAnswer = false;
+        }
     }
 }
